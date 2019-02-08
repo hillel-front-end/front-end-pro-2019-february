c0af93c4de0c774a7d5908f90dbbdd4dbbccecda
savelii.sychov <savelii.sychov@myheritage.com>
2019-02-08 09:52:12 +0200

    L_1

--- lections/lection_1_Intro/materials/main.js
+++ lections/lection_1_Intro/materials/main.js
@@ -1,1 +1,24 @@
-console.log("Lection 1");
+// a = 10;
+// b = 20;
+// c = a + b;
+// str = 'Hello world!';
+// console.log(c);
+// console.log(str);
+
+
+// console.log( a - b + c - a*2 );
+
+// ---------------------------------
+
+x = 1; // Number
+y = '4'; // String
+
+console.log( x - y );
+// y преобразуется в число
+
+console.log( x * y );
+console.log( x / y );
+console.log( x % y ); 
+
+
+console.log( x + y );
