97d1786e7bb273c838e0db522d49d538557a52aa
you@example.com <you@example.com>
2019-05-16 18:55:23 +0300

    L_25

---
+++ lections/lection_25_Основы NodeJS_Express.js примеры/materials/app.js
@@ -0,0 +1,24 @@
+console.log('app start')
+
+// systemJs
+// let post;
+
+// if(true){
+//     post = require('./post');
+// }
+
+const post = require('./post');
+
+
+// const enL10N = require('./en');
+// var jsonFormat = require('./jsonFormat');
+// document.write(10)
+var x = 100;
+function f(){
+    return {x: 10, name: 'Hello'};
+    return [2, 7, 8, 9, 54];
+}
+
+var res = f();
+console.log(res);
+console.log('app end');
